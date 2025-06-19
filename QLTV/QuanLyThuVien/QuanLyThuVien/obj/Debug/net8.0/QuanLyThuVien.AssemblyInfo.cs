9c75a1a11971833166bbf12808ff23c0ea0bf29d
thucnguyenhien102 <thucnguyenhien102@gmail.com>
2025-06-19 12:50:21 +0700

    up

--- QLTV/QuanLyThuVien/QuanLyThuVien/obj/Debug/net8.0/QuanLyThuVien.AssemblyInfo.cs
+++ QLTV/QuanLyThuVien/QuanLyThuVien/obj/Debug/net8.0/QuanLyThuVien.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyThuVien")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bebab98b37974c888645eccfc99df7ba308a0a9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyThuVien")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyThuVien")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
